2273be66fe53672783929ea658d203f4ebd5593d
JLionel <ljanin@gaming.tech>
2022-02-03 20:38:00 +0100

    last effort

--- projet-so6/Assets/Scripts/BehaviourTree/Actions/TankAction/MoveToNode.cs
+++ projet-so6/Assets/Scripts/BehaviourTree/Actions/TankAction/MoveToNode.cs
@@ -8,7 +8,9 @@ namespace BehaviourTree
         private TankPathSystem _tankPathSystem;
         private TankMovement _tankMovement;
         private TankManager _tankManager;
-        
+
+        private bool doNode;
+
         public string positionName;
         
         [Range(0.1f, 2f)]
@@ -20,12 +22,17 @@ namespace BehaviourTree
             
             _tankMovement = _tankManager.tankInstance.GetComponent<TankMovement>();
             _tankPathSystem = _tankManager.tankInstance.GetComponent<TankPathSystem>();
+            doNode = true;
             
             Vector3 targetPos = blackboard.GetValue<Vector3>(positionName);
             
             Transform currentTransform = _tankManager.tankInstance.transform;
             if (Vector3.SqrMagnitude(targetPos - currentTransform.position) < goalPrecision)
+            {
+                Debug.Log("Don't do node");
+                doNode = false;
                 return;
+            }
             
             _tankPathSystem.SearchTargetPath(targetPos);
         }
@@ -38,20 +45,34 @@ namespace BehaviourTree
 
         protected override State OnUpdate()
         {
+            if (!doNode)
+                return State.Success;
             
-            if (_tankPathSystem.MyPath.Count == 0)
-                return State.Running;
-
             Transform currentTransform = _tankManager.tankInstance.transform;
             
-            if (Vector3.SqrMagnitude(_tankPathSystem.MyPath.Last() - currentTransform.position) < goalPrecision)
+            if (_tankPathSystem.MyPath.Count > 0 && Vector3.SqrMagnitude(_tankPathSystem.MyPath.Last() - currentTransform.position) < goalPrecision)
+            {
+                Debug.Log("SUCCESS");
+                
+                _tankPathSystem.MyPath.Clear();
                 return State.Success;
+            }
+            
+            if (_tankPathSystem.MyPath.Count == 0)
+            {
+                Debug.Log("RUNNING");
+                return State.Running;
+            }
+
+            
+            _tankPathSystem.Agent.nextPosition = currentTransform.position;
 
             _tankPathSystem.Agent.nextPosition = currentTransform.position;
             
             Vector3 nextDestination = _tankPathSystem.MyPath[0];
             float angle = Vector3.SignedAngle(currentTransform.forward, nextDestination - currentTransform.position, Vector3.one);
             
+            Debug.Log(angle);
             _tankMovement.TurnInputValue = _tankPathSystem.TurnRateCurve.Evaluate(angle);
             _tankMovement.MovementInputValue = _tankPathSystem.MoveRateCurve.Evaluate(angle);
             
